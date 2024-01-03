c42776ed0ec1c191d427df801ebe66972b554ff5
Andrey Morskov <accessd0@gmail.com>
2024-01-03 20:52:44 +0200

    remove comments

--- terminal_sunday.sh
+++ terminal_sunday.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# Определение цветов для вывода
 RED="\033[41m  \033[0m"
 GREEN="\033[42m  \033[0m"
-GAP=" " # Зазор между элементами
+GAP=" "
 
-# Проверка наличия необходимых аргументов
 if [ $# -lt 1 ]; then
   echo "Usage: $0 birthdate [username]"
   echo "Example: $0 1985-06-08 Joe"
   exit 1
 fi
 
-birthdate=$1 # Первый аргумент - дата рождения
-name=${2:-$USER} # Второй аргумент - имя пользователя, по умолчанию $USER
+birthdate=$1
+name=${2:-$USER}
 
-life_expectancy=80     # Ожидаемая продолжительность жизни в годах
+life_expectancy=80
 last_year_index=$((life_expectancy - 1))
 
 case "$(uname)" in
@@ -37,10 +35,8 @@ weeks_passed=$(( (current_timestamp - birth_timestamp) / 604800 ))
 total_weeks=$((life_expectancy * 52))
 weeks_remaining=$((total_weeks - weeks_passed))
 
-# Выводим имя и оставшееся количество недель
 echo -e "$name, only $weeks_remaining Sundays remain\n"
 
-# Выводим сетку с группировкой по годам 20x4
 for (( row=0; row<4; row++ )); do
     for (( col=0; col<20; col++ )); do
         year_index=$((row * 20 + col))
@@ -48,14 +44,12 @@ for (( row=0; row<4; row++ )); do
             echo $birth_year
           fi
         if (( year_index < years_passed )); then
-            # Прошедшие годы закрашиваем красным
             echo -ne "${RED}${GAP}"
         else
-            # Еще не прошедшие годы закрашиваем зеленым
             echo -ne "${GREEN}${GAP}"
         fi
     done
-    echo # Новая строка после каждой строки сетки
+    echo
     if (( year_index != $last_year_index )); then
       echo
     fi
